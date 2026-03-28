c5015927fb9d59af255fb836967ef02b10bdded1
Tsaqif Afif Ashiilah <afifashiilah@gmail.com>
2026-03-28 21:06:51 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022430004/Program.cs
+++ TP_MODUL5_103022430004/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+class main
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        Console.Write("Masukkan nama panggilan Anda: ");
+        string namaPanggilan = Console.ReadLine();
+        halo.SapaUser(namaPanggilan);
+        Console.WriteLine();
+        Console.Write("Masukkan NIM: ");
+        string nim = Console.ReadLine();
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        data.PrintData();
+    }
+}
